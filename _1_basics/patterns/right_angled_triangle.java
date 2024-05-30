cd2cf653af08ce6938563431a081a4ee4e911b29
HarshitKachhadiya <157937871+harshit-hk133@users.noreply.github.com>
2024-05-30 12:13:24 +0530

    Update right_angled_triangle.java

--- _1_basics/patterns/right_angled_triangle.java
+++ _1_basics/patterns/right_angled_triangle.java
@@ -1,6 +1,5 @@
 public class Solution {
     public static void nForest(int n) {
-        // Write your code here
         for(int i = 0; i < n ; i++) {
             for(int j = 0; j <= i; j++) {
                 System.out.print("* ");
