8ed601ccde25de7744139c684701bd7dffaa3b0f
Harshit Kachhadiya <157937871+harshit-hk133@users.noreply.github.com>
2024-07-19 12:03:16 +0530

    Update inverted_right_pyramid.java

--- _1_basics/patterns/inverted_right_pyramid.java
+++ _1_basics/patterns/inverted_right_pyramid.java
@@ -1,1 +1,26 @@
+// Problem Definition : 
+// Sam is planting trees on the upper half region (separated by the left diagonal) of the square shared field.
 
+// For every value of ‘N’, print the field if the trees are represented by ‘*’.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Code :
+  
+public class Solution {
+    public static void seeding(int n) {
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output : 
+
+// * * * 
+// * * 
+// * 
