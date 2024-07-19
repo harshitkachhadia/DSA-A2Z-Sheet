9bd6147560ca1cb9dc3647a1fd598b79868ceeb0
Harshit Kachhadiya <157937871+harshit-hk133@users.noreply.github.com>
2024-07-19 12:09:59 +0530

    Update right_angled_triangle.java

--- _1_basics/patterns/right_angled_triangle.java
+++ _1_basics/patterns/right_angled_triangle.java
@@ -1,3 +1,13 @@
+// Problem Definition :
+// Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+// An N/2-dimensional forest is represented by the lower triangle of the pattern filled with ‘*’.
+// For every value of ‘N’, help sam to print the corresponding N/2-dimensional forest.
+
+// Example:
+// Input:  ‘N’ = 3
+
+// Code :
+
 public class Solution {
     public static void nForest(int n) {
         for(int i = 0; i < n ; i++) {
@@ -8,3 +18,8 @@ public class Solution {
         }
     }
 }
+
+// Output: 
+// * 
+// * *
+// * * *
