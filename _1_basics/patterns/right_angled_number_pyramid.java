b9579e026a123b5814f022d396ab2b1d82ff3baf
Harshit Kachhadiya <157937871+harshit-hk133@users.noreply.github.com>
2024-07-19 12:06:43 +0530

    Update right_angled_number_pyramid.java

--- _1_basics/patterns/right_angled_number_pyramid.java
+++ _1_basics/patterns/right_angled_number_pyramid.java
@@ -1,3 +1,15 @@
+// Problem Definition : 
+// Sam is making a Triangular painting for a maths project.
+
+// An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers starting from 1.
+
+// For every value of ‘N’, help sam to print the corresponding N-dimensional Triangle.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Code :    
+
 public class Solution {
     public static void nTriangle(int n) {
         for(int i = 0; i < n; i++){
@@ -11,3 +23,10 @@ public class Solution {
         }
     }
 }
+
+
+// Output: 
+// 1
+// 1 2 
+// 1 2 3
+
