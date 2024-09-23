b03182faaf9d7b9395178d621fc38306bdd6f764
Harshit Kachhadiya <157937871+harshitkachhadia@users.noreply.github.com>
2024-09-23 15:22:24 +0530

    Create array_is_sorted.java

---
+++ _2_Array/Easy/array_is_sorted.java
@@ -0,0 +1,10 @@
+class Solution {
+  static boolean isSorted(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+      if (arr[i] < arr[i - 1])
+        return false;
+    }
+
+    return true;
+  }
+}
