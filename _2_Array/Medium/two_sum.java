a8a5391d131d31f6dec2d8658080cdad97d5dba2
Harshit Kachhadiya <157937871+harshitkachhadia@users.noreply.github.com>
2024-10-16 18:19:11 +0530

    Update two_sum.java

--- _2_Array/Medium/two_sum.java
+++ _2_Array/Medium/two_sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       int[] ans = new int[2];
-       ans[0] = ans[1] = -1;
+       
        HashMap<Integer, Integer> map = new HashMap<>();
        for(int i = 0; i < nums.length; i++) {
             int num = nums[i];
