ee8e06ef481cb9944f39f69a089aa42ca8e3aaa7
Harshit Kachhadiya <157937871+harshitkachhadia@users.noreply.github.com>
2024-10-18 11:12:57 +0530

    Update stock_buy_and_sell.java

--- _2_Array/Medium/stock_buy_and_sell.java
+++ _2_Array/Medium/stock_buy_and_sell.java
@@ -1,3 +1,5 @@
+// You want to maximize your profit by choosing a single day to buy
+// one stock and choosing a different day in the future to sell that stock.
 import java.util.*;
 class Solution {
     public int maxProfit(int[] prices) {
